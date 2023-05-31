a206b133a15ed6beb5d3b8f3122cfc030b9e7ac2
douglascaique <60799530+douglascaique@users.noreply.github.com>
2023-05-31 02:15:32 -0300

    Estudos JS Dio

---
+++ JS DIO/aula.js
@@ -0,0 +1,17 @@
+
+// serve para imprimir coisa no terminal, é uma interface que diz "imprima"
+console.log("Imprimir algo")
+
+//Variaveis são espaços para guardar valor
+//Temos duas formas de declarar variaveis no JSs
+
+let variavel = 10;  // let pode ser alterada em outros momentos do codigo
+console.log(variavel)
+
+variavel = 20; // posso realizar alterações
+console.log(variavel)
+
+
+const pi = 3.14;   //const nao pode ser mudado depois de ser declarado
+
+//    + - * /
